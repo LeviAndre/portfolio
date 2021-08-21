43a072517e529a81578842222eb8ef90e32fe3f7
LeviAndre <andrelevi767@gmail.com>
2021-08-21 07:48:17 -0300

    responsive and js fixes

---
+++ script.js
@@ -0,0 +1,17 @@
+function headerAnim() {
+
+var prevScrollpos = window.pageYOffset;
+const headerPos = document.querySelector('.js-header');
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    headerPos.classList.remove('ativo');
+  } else {
+    headerPos.classList.add('ativo');
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+}
+
+headerAnim();
