6a4ea3844011a04efc5a07d8d3715f7f67e77d2e
Andre Levi <andre.levi@novafutura.com.br>
2023-08-26 14:08:45 -0300

    whole architecture changed to a react app

--- modules/script.js
+++
@@ -1,5 +0,0 @@
-import { scrollAnim } from './scroll.js';
-import { headerAnim } from './header.js';
-
-scrollAnim();
-headerAnim();
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import Routes from "config/routes";
+import React from "react";
+// import "./globalStyles.css";
+import Intro from "./pages/Intro/Intro.jsx"
+
+function App () {
+  return (
+    <>
+      <Intro />
+    </>
+  );
+};
+
+export default App;
